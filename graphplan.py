db5d9633161221ae0c754985bd3612a7741b411f
BigBaz <louisvincent@famillecapelli.fr>
2024-03-25 14:30:56 -0400

    [~] mutex pairs are sets

--- graphplan.py
+++ graphplan.py
@@ -1,19 +1,19 @@
 from domain import RocketDomain
 
 class GraphPlan:
+    def __init__(self, r_fact):
+        self.rd = RocketDomain(r_fact)
+        self.layers = []
 
 
-
-
-    
     def get_producers(self, proposition, actions):
         """
-        Returns a list of actions that have the given proposition as a positive effect.
+        Returns a set of actions that have the given proposition as a positive effect.
         :param proposition: Proposition object
         :param actions: list of Action objects
         :return: list
         """
-        return [action for action in actions if proposition in action.positive_effects]
+        return {action for action in actions if proposition in action.positive_effects}
     
     def are_mutex_actions(self, action1, action2, mutex_propositions):
         if action1 == action2:
@@ -22,25 +22,25 @@ class GraphPlan:
             return True
         for prop1 in action1.preconditions:
             for prop2 in action2.preconditions:
-                if (prop1, prop2) in mutex_propositions:
+                if {prop1, prop2} in mutex_propositions:
                     return True
         return False
 
     def are_mutex_propositions(self, prop1, prop2, mutex_actions):
         for action1 in self.get_producers(prop1, self.actions):
             for action2 in self.get_producers(prop2, self.actions):
-                if (action1, action2) not in mutex_actions:
+                if {action1, action2} not in mutex_actions:
                     return False
         return True
 
     def get_mutex_actions(self, actions, mutex_propositions):
         """
-        Returns a set of tuples of actions that are mutex.
+        Returns a set of set of actions that are mutex.
         :param actions: list of Action objects
         :param mutex_propositions: list of tuples of Proposition objects
         :return: list
         """
-        return {(action1, action2) for action1 in actions for action2 in actions if self.are_mutex_actions(action1, action2, mutex_propositions)}
+        return {{action1, action2} for action1 in actions for action2 in actions if self.are_mutex_actions(action1, action2, mutex_propositions)}
 
     def get_mutex_propositions(self, propositions, mutex_actions):
         """
@@ -49,4 +49,4 @@ class GraphPlan:
         :param mutex_actions: list of tuples of Action objects
         :return: list
         """
-        return {(prop1, prop2) for prop1 in propositions for prop2 in propositions if self.are_mutex_propositions(prop1, prop2, mutex_actions)}
+        return {{prop1, prop2} for prop1 in propositions for prop2 in propositions if self.are_mutex_propositions(prop1, prop2, mutex_actions)}
